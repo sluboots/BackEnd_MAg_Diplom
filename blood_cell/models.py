84833e49615e8ae8c80b527c2543b0455cb61d37
sluboots <sluboots@gmail.com>
2023-05-22 22:43:23 +0500

    Add Blood Cell and Leukocytes Cell Apps and create models for this apps

---
+++ blood_cell/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from user.models import Profile
+
+# Create your models here.
+class Blood_Cell(models.Model):
+    file = models.ImageField(upload_to='files/', null=True, max_length=255)
+    target = models.BooleanField(null = True, blank=True)
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
+
---
+++ leykocutes_cell/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LeykocutesCellConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'leykocutes_cell'
